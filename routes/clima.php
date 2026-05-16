7f4841acc5fe14ece8693705777c1e5fd047996a
jp085 <150303340+jp085@users.noreply.github.com>
2026-05-16 20:48:52 -0300

    Create clima.php

---
+++ routes/clima.php
@@ -0,0 +1,85 @@
+<?php
+
+header('Content-type: application/json');
+
+date_default_timezone_set("America/Sao_Paulo");
+
+$url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$UrlExplode = explode("/", $_GET['path']);
+
+try {
+    
+    @$UrlExplode[2];
+    $response1 = @file_get_contents("https://brasilapi.com.br/api/cptec/v1/cidade/$UrlExplode[2]");
+    $decode1 = json_decode($response1,    true);
+    $status = $http_response_header[0];
+    
+    if($response1 == True){
+    
+   
+        foreach($decode1 as $decode){
+
+        if(str_contains($status, "503")){
+            $resposta503 = [
+                "erro"     => True,
+                "codigo"   => "SERVICO_EXTERNO_INDISPONIVEL",
+                "mensagem" => "Não foi possível obter dados do serviço externo. Tente novamente em alguns instantes",
+                "servico"  => "CPTEC"
+            ];
+            echo json_encode($resposta503, JSON_PRETTY_PRINT);
+            break;
+        }
+
+          elseif(strlen($UrlExplode[2]) >= 2){
+            $id = $decode['id'];
+            $response2 = file_get_contents("https://brasilapi.com.br/api/cptec/v1/clima/previsao/$id");
+            $decode2 = json_decode($response2, true);
+            echo json_encode($decode2,  JSON_PRETTY_PRINT);
+            
+        }
+
+        elseif(strlen($UrlExplode[2]) < 2){
+           $resposta400 = [
+            "erro"=> true,
+            "codigo"=> "NOME_INVALIDO",
+            "mensagem"=> "O nome da cidade deve conter pelo menos 2 caracteres",
+            "nome_informado"=> $UrlExplode[2]
+           ];
+           echo json_encode($resposta400);
+            break;
+
+        }   
+
+    };
+            
+    }
+
+    elseif($response1 == False){
+        if($UrlExplode[2]){
+                $resposta404 = [
+                "erro" => true,
+                "codigo" => "CIDADE_NAO_ENCONTRADA",
+                "mensagem" => "Nenhuma cidade encontrada com o nome informado",
+                "nome_informado" => $UrlExplode[2]
+            ];  
+            echo json_encode($resposta404);
+        }
+        else{
+        $resposta404 = [
+                "erro" => true,
+                "codigo" => "CIDADE_NAO_ENCONTRADA",
+                "mensagem" => "Nenhuma cidade encontrada com o nome informado",
+                "nome_informado" => $UrlExplode[2]
+            ];  
+            echo json_encode($resposta404);
+        }
+        
+    }
+    
+} catch (PDOException $e){
+    echo json_encode(['error' => $e->getMessage()]);
+
+}
+
+?>
