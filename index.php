b850201063054b39e6f46b0a92bceb6bedf13a66
jp085 <150303340+jp085@users.noreply.github.com>
2026-05-15 23:32:33 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,19 +8,52 @@ $UrlExplode = explode("/", $_GET['path']);
 try {
     
     $UrlExplode[1];
-    $response1 = file_get_contents("https://brasilapi.com.br/api/cptec/v1/cidade/$UrlExplode[1]");
+    $response1 = @file_get_contents("https://brasilapi.com.br/api/cptec/v1/cidade/$UrlExplode[1]");
     $decode1 = json_decode($response1,    true);
-    foreach($decode1 as $decode){
-        /*echo strtolower($valor2['nome']);
-        echo " ";
-        echo $UrlExplode[1];"*/
-        if(strtolower($decode['nome']) == strtolower($UrlExplode[1])){
+    if($response1 == True){
+        foreach($decode1 as $decode){
+       
+       /* if(strtolower($decode['nome']) == strtolower($UrlExplode[1])){
             $id = $decode['id'];
             $response2 = file_get_contents("https://brasilapi.com.br/api/cptec/v1/clima/previsao/$id");
             $decode2 = json_decode($response2, true);
-            var_dump($decode2);
+            echo json_encode($decode2,JSON_PRETTY_PRINT);
+            
+        }*/
+
+          if(strlen($UrlExplode[1]) >= 2){
+            $id = $decode['id'];
+            $response2 = file_get_contents("https://brasilapi.com.br/api/cptec/v1/clima/previsao/$id");
+            $decode2 = json_decode($response2, true);
+            echo json_encode($decode2,  JSON_PRETTY_PRINT);
+            break;
         }
+        elseif(strlen($UrlExplode[1]) < 2){
+           $resposta400 = [
+            "erro"=> true,
+            "codigo"=> "NOME_INVALIDO",
+            "mensagem"=> "O nome da cidade deve conter pelo menos 2 caracteres",
+            "nome_informado"=> $UrlExplode[1]
+           ];
+           echo json_encode($resposta400);
+            break;
+
+        }   
+
+    };
+            
     }
+    elseif($response1 == False){
+        $resposta404 = [
+                "erro" => true,
+                "codigo" => "CIDADE_NAO_ENCONTRADA",
+                "mensagem" => "Nenhuma cidade encontrada com o nome informado",
+                "nome_informado" => $UrlExplode[1]
+            ];
+            echo json_encode($resposta404);
+    }
+
+
     
 } catch (PDOException $e){
     echo json_encode(['error' => $e->getMessage()]);
