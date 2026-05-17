28bc737f60661d57bf4d67424a8ee60a3a7d6991
jp085 <150303340+jp085@users.noreply.github.com>
2026-05-17 11:29:18 -0300

    Update clima.php

--- routes/clima.php
+++ routes/clima.php
@@ -15,31 +15,42 @@ try {
     $decode1 = json_decode($response1,    true);
     $status = $http_response_header[0];
     
-    if($response1 == True){
-    
+    if(empty($UrlExplode[2])){
+        foreach($decode1 as $decode){
+            $id = $decode['id'];
+            $response2 = file_get_contents("https://brasilapi.com.br/api/cptec/v1/clima/previsao/$id");
+            $decode2 = json_decode($response2, true);
+            echo json_encode($decode2, JSON_UNESCAPED_UNICODE);
+            exit;
+      }
+    }
+
+    if($response1 !== False){
    
         foreach($decode1 as $decode){
 
         if(str_contains($status, "503")){
+            http_response_code(503);
             $resposta503 = [
                 "erro"     => True,
                 "codigo"   => "SERVICO_EXTERNO_INDISPONIVEL",
                 "mensagem" => "Não foi possível obter dados do serviço externo. Tente novamente em alguns instantes",
                 "servico"  => "CPTEC"
             ];
-            echo json_encode($resposta503, JSON_PRETTY_PRINT);
-            break;
+            echo json_encode($resposta503, JSON_UNESCAPED_UNICODE);
+            exit;
         }
 
           elseif(strlen($UrlExplode[2]) >= 2){
             $id = $decode['id'];
             $response2 = file_get_contents("https://brasilapi.com.br/api/cptec/v1/clima/previsao/$id");
             $decode2 = json_decode($response2, true);
-            echo json_encode($decode2,  JSON_PRETTY_PRINT);
-            
+            echo json_encode($decode2, JSON_UNESCAPED_UNICODE);
+            exit;
         }
 
         elseif(strlen($UrlExplode[2]) < 2){
+        http_response_code(400);
            $resposta400 = [
             "erro"=> true,
             "codigo"=> "NOME_INVALIDO",
@@ -47,7 +58,7 @@ try {
             "nome_informado"=> $UrlExplode[2]
            ];
            echo json_encode($resposta400);
-            break;
+            exit;
 
         }   
 
@@ -56,6 +67,7 @@ try {
     }
 
     elseif($response1 == False){
+        http_response_code(404);
         if($UrlExplode[2]){
                 $resposta404 = [
                 "erro" => true,
@@ -64,6 +76,7 @@ try {
                 "nome_informado" => $UrlExplode[2]
             ];  
             echo json_encode($resposta404);
+            exit;
         }
         else{
         $resposta404 = [
@@ -73,11 +86,12 @@ try {
                 "nome_informado" => $UrlExplode[2]
             ];  
             echo json_encode($resposta404);
+            exit;
         }
         
     }
     
-} catch (PDOException $e){
+} catch (Exception $e){
     echo json_encode(['error' => $e->getMessage()]);
 
 }
