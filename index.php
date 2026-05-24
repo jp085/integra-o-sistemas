7e24b5a5339676ea1190e1a7dcf67ed93b10cc07
jp085 <150303340+jp085@users.noreply.github.com>
2026-05-23 21:25:50 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,29 @@ date_default_timezone_set("America/Sao_Paulo");
 
 $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
+if(!isset($_GET['path'])){
+    $_GET['path'] = '';
+} 
+
 $UrlExplode = explode("/", $_GET['path']);
 
-switch($UrlExplode[1]){
 
+if(!isset($UrlExplode[1])){
+    $UrlExplode[1] = '';
+} 
+
+switch($UrlExplode[1]){
+    case '':
+        echo json_encode([
+            "api"       => "online",
+            "version"   => "1.0",
+            "endpoints" => [
+                "/clima",
+                "/cidades",
+                "/health"
+            ]
+        ]);
+        exit;
     case 'clima':
         require 'routes/clima.php';
         break;
